b6708d3de387b24e34ba570aca9d4611e17d62cb
yangjianfan <584241195@qq.com>
2018-09-07 18:00:43 +0800

    引入包

---
+++ techsys-entity/src/main/java/com/jeffrey/entity/sysUser/sysUser.java
@@ -0,0 +1,51 @@
+package com.jeffrey.entity.sysUser;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name="sys_user")
+@Entity
+public class sysUser {
+
+
+    private Integer userId;
+
+    private String userName;
+
+    private String account;
+
+    private String password;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
